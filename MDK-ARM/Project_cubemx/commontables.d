0531488e61abbee49674812f9924b66cf4dcbf0c
PetrDYu <68391651+PetrDYu@users.noreply.github.com>
2022-07-03 17:34:06 +0300

    Add project files

---
+++ MDK-ARM/Project_cubemx/commontables.d
@@ -0,0 +1,16 @@
+project_cubemx\commontables.o: C:\Users\petry\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\DSP\Source\CommonTables\CommonTables.c
+project_cubemx\commontables.o: C:\Users\petry\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\DSP\Source\CommonTables\arm_common_tables.c
+project_cubemx\commontables.o: ../Drivers/CMSIS/DSP/Include/arm_math.h
+project_cubemx\commontables.o: ../Drivers/CMSIS/Include/core_cm4.h
+project_cubemx\commontables.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+project_cubemx\commontables.o: ../Drivers/CMSIS/Include/cmsis_version.h
+project_cubemx\commontables.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+project_cubemx\commontables.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+project_cubemx\commontables.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+project_cubemx\commontables.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+project_cubemx\commontables.o: ../Drivers/CMSIS/DSP/Include/arm_common_tables.h
+project_cubemx\commontables.o: ../Drivers/CMSIS/DSP/Include/arm_math.h
+project_cubemx\commontables.o: C:\Users\petry\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\DSP\Source\CommonTables\arm_const_structs.c
+project_cubemx\commontables.o: ../Drivers/CMSIS/DSP/Include/arm_math.h
+project_cubemx\commontables.o: ../Drivers/CMSIS/DSP/Include/arm_const_structs.h
+project_cubemx\commontables.o: ../Drivers/CMSIS/DSP/Include/arm_math.h
---
+++ MDK-ARM/Project_cubemx/startup_stm32f401xe.d
@@ -0,0 +1,1 @@
+project_cubemx\startup_stm32f401xe.o: startup_stm32f401xe.s
